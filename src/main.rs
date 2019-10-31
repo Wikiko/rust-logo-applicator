b1ff414fc7649074637974791655854d41cd4fe8
William Azevedo <willianray@gmail.com>
2019-10-31 17:06:51 -0300

    Adicionado novas dependencias e processamento paralelo com threadpool.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
-use raster::editor;
+use raster::{editor, ResizeMode};
 use raster::BlendMode;
 use raster::PositionMode;
 use std::fs::read_dir;
 use std::io;
+use threadpool::ThreadPool;
 
 fn main() {
-
-    // let image1 = raster::open("sample.jpg").unwrap();
-    // let image2 = raster::open("watermark.png").unwrap();
-
-    // let image3 = editor::blend(&image1, &image2, BlendMode::Normal, 1.0, PositionMode::BottomRight, -15, -15).unwrap();
-
-    // raster::save(&image3, "watermarked.jpg").unwrap();
-
-    
     apply_watermark().unwrap();
-
 }
 
 fn apply_watermark() -> io::Result<()> {
-    let watermark = raster::open("watermark.png").unwrap();
+    let total_cpus = num_cpus::get();
+    println!("Numero de processadores: {}", total_cpus);
+    let pool = ThreadPool::new(total_cpus);
 
     for (i, entry) in read_dir("./photos")?.enumerate() {
         let entry = entry?;
@@ -29,16 +22,20 @@ fn apply_watermark() -> io::Result<()> {
             continue;
         }
 
-        let image = raster::open(&path).unwrap();
+        pool.execute(move || {
+            let image = raster::open(&path).unwrap();
+            let mut watermark = raster::open("watermark.png").unwrap();
 
-        // let watermarked_image = editor::blend(&image, &watermark, BlendMode::Normal, 1.0, PositionMode::Center, 0, 0).unwrap();
+            let teste = format!("{}.jpg", &i);
+            let file_name = entry.path().file_name().and_then(|inner_path| inner_path.to_str()).map(|s| String::from(s)).unwrap_or(teste);
+            editor::resize(&mut watermark, 500, 500, ResizeMode::Fit);
+            let watermarked_image = editor::blend(&image, &watermark, BlendMode::Normal, 1.0, PositionMode::Center, 0, 0).unwrap();
+            raster::save(&watermarked_image, &file_name).unwrap();
+            println!("Saved {:?}", &file_name);
+        });
 
-        // raster::save(&watermarked_image, &path).unwrap();
-        let teste = format!("{}.jpg", &i);
-        let file_name = entry.path().file_name().and_then(|inner_path| inner_path.to_str()).map(|s| String::from(s)).unwrap_or(teste);
-        raster::save(&image, &file_name);
-        println!("Saved {:?}", &file_name);
     }
 
+    pool.join();
     Ok(())
 }
